f8055883f3388ec38889f85d5d73b668a64d84eb
Alejandro Leonardo García Navarro <al.garcia636@gmail.com>
2024-07-22 18:03:36 +0200

    organized files in a general folder

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 library('haven')
 library('dplyr')
 
-fn <- read_sav('data/fake_news.sav')
+fn <- read_sav('resources/fake_news.sav')
 head(fn)
 
 names(fn)
@@ -16,4 +16,4 @@ fn[, 'AFFECTIVE_POLARIZATION']
 variable_labels <- sapply(fn, function(x) attr(x, "label"))
 variable_table <- data.frame(Column_Name = names(variable_labels), Variable_Label = variable_labels)
 print(variable_table)
-write.csv(variable_table, "data/questions.csv", row.names = FALSE)
+write.csv(variable_table, "resources/questions.csv", row.names = FALSE)
