28235f838c9283fc67ad4cf0ca7db6dfd489000e
algar <al.garcia636@gmail.com>
2024-08-27 10:08:01 +0200

    troubleshooting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import numpy as np
 import streamlit as st
 
-!pip install ipython
+pip install ipython
 from IPython import display
 from concordia import typing
 from concordia.agents import basic_agent
