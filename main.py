6fa5929f40a1856ebd3b43c3f17e53e6aacc85ed
algar <al.garcia636@gmail.com>
2024-11-17 12:49:14 +0100

    hide the sidebar and removed start.py (moved everything to details.py)

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import streamlit as st
 
+# Hide the sidebar
+st.set_page_config(page_title="main", layout="wide", initial_sidebar_state="collapsed")
+
 # Load the CSS file and apply the styles
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Dictionary mapping page names to functions
 pages = {
-    "Start": "./pages/start.py",
+    "Details": "./pages/details.py",
 }
 
 # Header
 st.markdown("""<h1>Ready to explore Generative Agent-Based Modeling?</h1>""", unsafe_allow_html=True)
 
 # Button
-start_button = st.button("Let's Go!")
-if start_button:
-    # Switch to the "Start" page
-    page_file = pages["Start"]
+details_button = st.button("Let's Go!")
+if details_button:
+    # Switch to the "Details" page
+    page_file = pages["Details"]
     st.switch_page(page_file)
--- pages/details.py
+++ pages/details.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
+# Dictionary mapping page names to functions
 pages = {
     "Home": "main.py",
+    "Details": "./pages/details.py",
+    "Key Features": "./pages/features.py",
+    "About Us": "./pages/about_us.py"
 }
 
 st.markdown("<h3>About This Platform: A Hands-On Tool for GABM Experimentation</h3>", unsafe_allow_html=True)
@@ -13,9 +17,35 @@ st.write(
     """
 )
 
-# Buttons
-home_button = st.button("Home")
-if home_button:
-    # Switch to the selected page
-    page_file = pages["Home"]
-    st.switch_page(page_file)
+
+# Create n columns to place the buttons side by side
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    home_button = st.button("Home")
+    if home_button:
+        # Switch to the selected page
+        page_file = pages["Home"]
+        st.switch_page(page_file)
+
+with col2:
+    details_button = st.button("Details")
+    if details_button:
+        # Switch to the "About" page
+        page_file = pages["Details"]
+        st.switch_page(page_file)
+    
+
+with col3:
+    feature_button = st.button("Key Features")
+    if feature_button:
+        # Switch to the "Key Features" page
+        page_file = pages["Key Features"]
+        st.switch_page(page_file)
+
+with col4:
+    about_us_button = st.button("About Us")
+    if about_us_button:
+        # Switch to the "About Us" page
+        page_file = pages["About Us"]
+        st.switch_page(page_file)
