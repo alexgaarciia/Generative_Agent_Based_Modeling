d374b75852db04e9be540c9690d2fb34a4fb6eb9
algar <al.garcia636@gmail.com>
2024-11-23 10:08:04 +0100

    added explanation on GABM

--- pages/details.py
+++ pages/details.py
@@ -3,7 +3,6 @@ import streamlit as st
 # Dictionary mapping page names to functions
 pages = {
     "Home": "main.py",
-    "Details": "./pages/details.py",
     "Key Features": "./pages/features.py",
     "About Us": "./pages/about_us.py"
 }
@@ -16,7 +15,7 @@ st.markdown(
         background: radial-gradient(circle at 88% 102%,#3E3EDE, #BEE8DF, #60CAE8, #99B5D5)
     }
     </style>
-""", unsafe_allow_html=True
+    """, unsafe_allow_html=True
 )
 
 # Header
@@ -27,16 +26,26 @@ st.markdown(
 )
 
 # Details
-st.write(
+st.markdown(
     """
-    This platform is designed to help researchers and users with minimal programming experience create and experiment with
-    generative agent-based models (GABMs). Leveraging the Concordia framework, this tool makes it easy to build custom agents,
-    explore their attributes, and conduct reliable experiments, all through an accessible and user-friendly interface.
-    """
-)
+    <p style="width: 600px; height: 300px; ">
+        Generative Agent-Based Modeling (GABM) combines generative models and agent-based modeling to 
+        create realistic simulations of complex systems. By integrating AI-driven agents with a focus on individual behavior, 
+        GABM allows for the exploration of interactions and emergent patterns in situations ranging from social networks 
+        to ecological models. This approach empowers researchers to simulate various scenarios, understand system dynamics, 
+        and predict the outcomes of different interventions.
+    </p>
+    """, unsafe_allow_html=True)
+#st.write(
+#    """
+#    This platform is designed to help researchers and users with minimal programming experience create and experiment with
+#    generative agent-based models (GABMs). Leveraging the Concordia framework, this tool makes it easy to build custom agents,
+#    explore their attributes, and conduct reliable experiments, all through an accessible and user-friendly interface.
+#    """
+#)
 
 # Create n columns to place the buttons side by side
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     home_button = st.button("Home")
@@ -46,21 +55,13 @@ with col1:
         st.switch_page(page_file)
 
 with col2:
-    details_button = st.button("Details")
-    if details_button:
-        # Switch to the "About" page
-        page_file = pages["Details"]
-        st.switch_page(page_file)
-    
-
-with col3:
     feature_button = st.button("Key Features")
     if feature_button:
         # Switch to the "Key Features" page
         page_file = pages["Key Features"]
         st.switch_page(page_file)
 
-with col4:
+with col3:
     about_us_button = st.button("About Us")
     if about_us_button:
         # Switch to the "About Us" page
